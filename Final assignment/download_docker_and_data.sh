92e680ac55490e598016d881ce1e58b74ad40021
jeroenderuijter2002 <j.w.d.ruijter@student.tue.nl>
2026-03-14 14:04:31 +0100

    Fix download script

--- Final assignment/download_docker_and_data.sh
+++ Final assignment/download_docker_and_data.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
-#SBATCH --partition=gpu_mig
+#SBATCH --partition=gpu_a100
 #SBATCH --time=3:00:00
 
 # Pull container from dockerhub
@@ -11,5 +11,5 @@ apptainer pull container.sif docker://cclaess/5lsm0:v1
 
 # Use the huggingface-cli package inside the container to download the data
 mkdir -p data
-apptainer exec container.sif \
+apptainer exec --env-file .env container.sif \
     huggingface-cli download TimJaspersTue/5LSM0 --local-dir ./data --repo-type dataset
